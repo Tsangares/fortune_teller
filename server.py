67e857dfed17db980522c095228ab1958a17d573
Tsangares <tsangares@gmail.com>
2024-04-05 02:17:11 +0000

    updating

--- server.py
+++ server.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 import random
 import os
 import logging
-from fortune import create_fortune, text_to_wav, DEFAULT_PRE_PROMPT
-
+import json
+from fortune import DEFAULT_PRE_PROMPT,create_fortune_text, create_fortune
 # Basic startup
 load_dotenv()
 app = Flask(__name__)
@@ -17,7 +17,7 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 mongo = PyMongo(app)
 
-
+"""
 @app.route('/generate/')
 def generate_new_fortune(**kwargs):
     return {'success': None}
@@ -33,27 +33,45 @@ def speak():
     string = request.args.get('text','This is a test.')
     audio_data = text_to_wav(string)
     return f'<audio controls src="data:audio/wav;base64, {audio_data}"></audio>'
+"""
 
 @app.route('/calibrate/', methods=['GET'])
 def calibrate_fortune_teller():
-    return render_template('/pages/calibrate.html',default_pre_prompt=DEFAULT_PRE_PROMPT)
+    pre_prompt = request.args.get("pre_prompt",DEFAULT_PRE_PROMPT)
+    prompt = request.args.get("prompt", None)
+    name = request.args.get('name','')
+    tags = request.args.get('tags','')
+    if tags != '':
+        tags = ', '.join(json.loads(tags.replace("'",'"')))
+    return render_template('/pages/calibrate.html',pre_prompt=pre_prompt,prompt=prompt,name=name,tags=tags)
 
 @app.route('/calibrate/', methods=['POST'])
 def calibrate_fortune_teller_custom():
     pre_prompt = request.form.get('pre_prompt',DEFAULT_PRE_PROMPT)
     prompt = request.form.get('prompt',"Tell me a fortune")
-    fortune = create_fortune(query=prompt,pre_prompt=pre_prompt)
-    return render_template('/pages/calibrate.html',fortune=fortune,pre_prompt=pre_prompt,prompt=prompt)
+    name = request.form.get('name','')
+    tags = request.form.get('tags',[])
+    if tags != []:
+        tags = [tag.strip() for tag in tags.split(',')]
+    tags += ['calibrate','general']
+    fortune_text,audio_string = create_fortune(query=prompt,pre_prompt=pre_prompt,tags=tags,name=name)
+    tags = ', '.join(tags)
+    return render_template('/pages/calibrate.html',fortune=fortune_text,audio_string=audio_string,pre_prompt=pre_prompt,prompt=prompt,name=name,tags=tags)
 
 @app.route('/calibrate/save', methods=['POST'])
 def save_calibration():
-    response = mongo.db.prompts.insert_one(dict(request.form))
+    tags = request.form.get('tags',[])
+    if tags != []:
+        tags = [tag.strip() for tag in tags.split(',')]
+    oracle = dict(request.form)
+    oracle['tags'] = tags
+    response = mongo.db.prompts.insert_one(oracle)
     return "<h3>Prompt Saved! Go to archive to see all saved prompts.</h3>"
 
 @app.route('/calibrate/all', methods=['GET'])
 def calibration_see_all():
     cursor = mongo.db.prompts.find({})
-    prompts = [p for p in cursor]
+    prompts = reversed([p for p in cursor])
     return render_template("/pages/calibrate_all.html",prompts=prompts)
 
 def get_last_modified_random_fortune(**kwargs):
